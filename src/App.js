5097d563e27cb4acb514de5db97bb2f395be2abf
devops-sagar <pathaksagar16@gmail.com>
2023-11-12 16:15:00 -0500

    starting with Home section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import Header from './components/header/Header';
 import './App.css';
+import Header from './components/header/Header';
+import Home  from './components/home/Home';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+
+      <main className='main'>
+        <Home />
+      </main>
+    </>
+
+
   );
 }
 
---
+++ src/components/home/Social.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      
+      <a href="https://www.linkedin.com/in/sagar097" className="home__social-icon" target='_blank'>
+        <i class="uil uil-linkedin"></i>
+      </a>
+
+      <a href="https://github.com/devops-sagar" className="home__social-icon" target='_blank'>
+        <i class="uil uil-github"></i>
+      </a>
+
+      <a href="https://dribbble.com" className="home__social-icon" target='_blank'>
+        <i class="uil uil-dribbble"></i>
+      </a>
+
+    </div>
+  )
+}
+
+export default Social
