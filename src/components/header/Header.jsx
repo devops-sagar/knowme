f7fa7b44e943e72f34f78741b5a1cbf04c076999
devops-sagar <pathaksagar16@gmail.com>
2023-11-11 22:10:46 -0500

    working on header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
+  const[Toggle, showMenu] = useState(false);
   return (
     <header className="header">
       <nav className="nav container">
 
         <a href="index.html" className="nav__logo">Sagar</a>
 
-        <div className="nav__menu">
+        <div className={Toggle ? "nav__menu show__menu" : "nav__menu"}>
 
-          <ul className="nav__list grid">
+          <ul className="nav__list">
 
             <li className="nav__item">
-              <a href="#home" className="nav__link">
+              <a href="#home" className="nav__link active-link">
                 <i className="uil uil-estate nav__icon"></i> Home
               </a>   
             </li>
@@ -24,7 +25,7 @@ const Header = () => {
               </a>   
             </li>
 
-            <li className="nav__item">
+            <li className="nav__item">  
               <a href="#skills" className="nav__link">
                 <i className="uil uil-file-alt nav__icon"></i> Skills
               </a>   
@@ -49,10 +50,10 @@ const Header = () => {
             </li>
 
           </ul>
-          <i className="uil uil-times nav__close"></i>
+          <i className="uil uil-times nav__close" onClick={() => showMenu(!Toggle)}></i>
         </div>
 
-        <div className="nav__toggle">
+        <div className="nav__toggle" onClick={() => showMenu(!Toggle)}>
           <i className="uil uil-apps"></i>
         </div>
 
