9788cfc7d44231393e7bf488af800d16e177e274
devops-sagar <pathaksagar16@gmail.com>
2023-12-11 00:20:55 -0500

    almost there

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Header from './components/header/Header';
 import Home  from './components/home/Home';
 import About  from './components/about/About';
 import Skills from './components/skills/Skills';
+import Services from './components/services/Services';
+import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Home />
         <About />
         <Skills />
+        <Services />
+        <Qualification />
+        <Testimonials />
       </main>
     </>
 
---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,38 @@
+import Image1 from "../../assets/testimonial1.png";
+import Image2 from "../../assets/testimonial2.png";
+import Image3 from "../../assets/testimonial3.png";
+
+export const Data = [
+  {
+    id: 1,
+    image: Image1,
+    title: "Michelle Manriquaz",
+    position: "Tech Lead @ CSUSM",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 2,
+    image: Image2,
+    title: "Heema Ruparelia",
+    position: "Analyst @ TCS",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 3,
+    image: Image3,
+    title: "Sachin Sharma",
+    position: "Staff SDE @ IBM",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+  {
+    id: 4,
+    image: Image3,
+    title: "Deepankar Mullick",
+    position: "SDE @ Oracle",
+    description:
+      "A really good job, all aspects of the project were followed step by step and with good results.",
+  },
+];
